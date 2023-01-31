b4e9431f50cfd59f11cea0d46da1360ba595a208
Nikola Jelicic <jelicicnikola1@gmail.com>
2023-01-31 21:00:06 +0100

    change 3

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -6,8 +6,8 @@ const Statistic = () => {
     <div className=' bg-statistika bg-cover h-[1000px] bg-center text-center'> 
         
 
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center justify-center flex flex-col'>
-        <p className='font-velikaslova text-4xl md:text-6xl text-slate-400  mt-72'>Statistika za godinu dana rada udruženja</p>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center justify-center flex flex-col '>
+        <p className='font-velikaslova text-4xl md:text-6xl text-white  mt-[500px]'>Statistika za godinu dana rada udruženja</p>
         <div className='flex justify-between mt-10'>
           <div className='pr-3'>
             <p className='font-velikaslova text-2xl md:text-5xl text-white'>10+</p>
@@ -33,20 +33,20 @@ const Statistic = () => {
         </div>
         <div className='flex justify-center mt-4'>
         <div className='pr-3'>
-            <p className='font-velikaslova text-4xl md:text-5xl text-white'>150+</p>
+            <p className='font-velikaslova text-2xl md:text-5xl text-white'>150+</p>
             <p className='text-siva'>Duhovnih oblasti</p>
           </div>
           
           <div className='pr-2'>
-           <p className='font-velikaslova text-4xl md:text-5xl text-white'>300+</p>
+           <p className='font-velikaslova text-2xl md:text-5xl text-white'>300+</p>
            <p className='text-siva text-sm'>zajedničkih sprovedenih akcija za<br/> globalno dobro</p>
          </div>
          <div className='pr-2'>
-            <p className='font-velikaslova text-4xl md:text-5xl text-white'>500+</p>
+            <p className='font-velikaslova text-2xl md:text-5xl text-white'>500+</p>
             <p className='text-siva text-sm'>Aktivnih članova</p>
           </div>
           <div>
-           <p className='font-velikaslova text-4xl md:text-5xl text-white'>800+</p>
+           <p className='font-velikaslova text-2xl md:text-5xl text-white'>800+</p>
             <p className='text-siva text-sm'>Održanih kurseva</p>
           </div>
           </div>
